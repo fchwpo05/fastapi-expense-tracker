842954bd18efbff1092c1a87963e713e1fe34641
fchwpo05 <vanshnagpal050@gmail.com>
2026-01-04 20:22:17 +0530

    Added Db indexes

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -3,4 +3,4 @@ from app.api.v1.endpoints import users,expenses
 
 api_router = APIRouter()
 api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(expenses.router, tags=["Expenses"])
+api_router.include_router(expenses.router, tags=["expenses"])
--- app/db/models/expense.py
+++ app/db/models/expense.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, DateTime, Index
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
@@ -12,9 +12,14 @@ class Expense(Base):
     title = Column(String, nullable=False)
     amount = Column(Float, nullable=False)
     category = Column(String, nullable=True)
-
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
     user = relationship("User", back_populates="expenses")
+
+    __table_args__ = (
+        Index("idx_expenses_user_id", "user_id"),
+        Index("idx_expenses_created_at", "created_at"),
+        Index("idx_expenses_user_created", "user_id", "created_at"),
+    )
